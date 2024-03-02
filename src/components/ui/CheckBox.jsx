dc534917ef78e8424306a24627ab5f4bfe80a6f2
Aakarsha0409 <aakarsha97@gmail.com>
2024-03-03 03:35:47 +0530

    carousel change

--- src/components/ui/CheckBox.jsx
+++ src/components/ui/CheckBox.jsx
@@ -8,7 +8,7 @@ export default function CheckBox({ text }) {
     };
 
     return (
-        <div className="lg:w-[562px] w-[370px] p-4 flex gap-2 items-center bg-white border-[#FF6B00] border-dashed border-2 rounded-xl">
+        <div className="lg:w-[562px] w-[350px] p-4 flex gap-2 items-center bg-white border-[#FF6B00] border-dashed border-2 rounded-xl">
             <input
                 className={`h-5 w-5 appearance-none rounded active:outline-none focus:outline-none checked:bg-[#FF6B00]`}
                 type="checkbox"
